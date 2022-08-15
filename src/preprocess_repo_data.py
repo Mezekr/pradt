d7b209ae3c56eb9002a7bd1678b77d2ca4f2c4a4
Mezekr <mezekr.edu@gmail.com>
2022-08-15 12:23:22 +0200

    feat: add process_issue_pullrequest
    Processed the raw pull request and Issues. It separates the open,
    updated, and closed pull requests as well as the issues. Then creates
    time series data for each of them and finally merges the time series.

--- src/preprocess_repo_data.py
+++ src/preprocess_repo_data.py
@@ -106,6 +106,83 @@ class RowRepoDataProcessor:
         print("Forks processed successfully")
         return feat_resample
 
+    def process_issue_pullrequest(
+        self, feature_name: str, repo_dir: str, save_path: str
+    ) -> Union[pd.DataFrame, pd.Series]:
+        """Returns or creates a DataFrame of the issue pull request time series Data.
+
+        Args:
+            feature_name (str): Feature(isse_pullrequest) of repository to be processed.
+            repo_dir (str): Directory path of the repository to be processed.
+            save_path (str):  Directory path to save issues and pull requests time series data.
+
+        Returns:
+            Union[pd.DataFrame, None]: Returns Dataframe of the Stars time series data.
+        """
+
+        # save_to: Path = Path(save_path)
+        # if not (repo_path.exists() and save_to.exists()):
+        #     raise NotFoundError(
+        #         f'Path Does not Exist: "{repo_path} or {save_to}" not found.'
+        #     )
+        repo_path, save_to = utils.set_path(repo_dir, save_path)
+        raw_file = utils.get_feat_file("issues_pulls", repo_path)
+
+        feat_resample = pd.read_csv(
+            raw_file,
+            parse_dates=[
+                "pr_iss_opened_at",
+                "pr_iss_updated_at",
+                "pr_iss_closed_at",
+            ],
+            infer_datetime_format=True,
+        )
+
+        pris_open_df = feat_resample.groupby(
+            pd.Grouper(key="pr_iss_opened_at", freq="D")
+        )
+        pris_grp_open_df = (
+            pris_open_df["pr_iss_opened_at"].count().to_frame(name="pr_iss_Open_count")
+        )
+
+        pris_lt_update = feat_resample.groupby(
+            pd.Grouper(key="pr_iss_updated_at", freq="D")
+        )
+        pris_grp_ltpdate_df = (
+            pris_lt_update["pr_iss_updated_at"]
+            .count()
+            .to_frame(name="pr_iss_updated_count")
+        )
+        pris_closed_df = feat_resample.groupby(
+            pd.Grouper(key="pr_iss_closed_at", freq="D")
+        )
+        pris_grp_closed_df = (
+            pris_closed_df["pr_iss_closed_at"]
+            .count()
+            .to_frame(name="pr_is_closed_count")
+        )
+
+        ps_merged_df = pd.merge(
+            pris_grp_open_df,
+            pris_grp_ltpdate_df,
+            how="outer",
+            left_index=True,
+            right_index=True,
+        )
+        ps_merged_df = pd.merge(
+            ps_merged_df,
+            pris_grp_closed_df,
+            how="outer",
+            left_index=True,
+            right_index=True,
+        )
+        processed_file_path = utils.get_save_path(
+            feature_name, repo_path, save_to, True
+        )
+        ps_merged_df.to_csv(processed_file_path)
+
+        return ps_merged_df
+
 
 @hydra.main(config_path="conf", config_name="config", version_base=None)
 def main(cfg: ReposConfig):
