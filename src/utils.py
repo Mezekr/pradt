e10170a9684811532a8139eadb11d7e3484189f8
Mezekr <mezekr.edu@gmail.com>
2022-08-15 12:01:24 +0200

    feat: add list_repos_dirs to list repos in dir

--- src/utils.py
+++ src/utils.py
@@ -37,3 +37,37 @@ def get_repo_path(repos_dir: Union[Path, str]) -> Optional[Path]:
         print(e)
 
         sys.exit(1)
+
+
+def list_repos_dirs(repos_dir: Path) -> List[Path]:
+    """Returns a list paths all directory of a repository in a directory.
+
+    Args:
+        repos_dir (str): Path of Directory of the repositories.
+
+    Raises:
+        NotFoundError: If the Directory is not availably.
+
+    Returns:
+        List[Path]: Repositoties path list.
+    """
+
+    # repos_path: Path = Path(repos_dir)
+    # if not get_repo_path(repos_path):
+    #     raise NotFoundError(f'Path Does not Exist: "{repos_path}" not found.')
+    # elif len(list(repos_path.iterdir())) == 0:
+    #     raise FileDirEmptyError(f"Empty File or Directory:{repos_path}")
+    repos_path = get_repo_path(repos_dir)
+    if repos_path is None:
+        print("Something went wrong")
+    dirs_list = {dir.name: dir for dir in repos_path.iterdir() if dir.is_dir()}
+    # print(dirs_list)
+    repo_dir = []
+    for dir_path in dirs_list.values():
+        if dir_path.is_dir():
+            subdir = [dir for dir in dir_path.iterdir() if dir.is_dir()]
+            if len(subdir) == 0:
+                repo_dir.append(dir_path)
+            repo_dir.extend([dir for dir in dir_path.glob("*") if dir.is_dir()])
+
+    return repo_dir
